ff921dcc716c63cee81cb279a094cabcab3a033e
pakagrong-lebel <92211586+pakagrong-lebel@users.noreply.github.com>
2023-10-29 15:47:42 +1100

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,3 @@
+# Python script to automate uploading files to cloud storage
+# Your code here to connect to a cloud storage service (e.g., AWS S3, Google Cloud Storage)
+# Your code here to upload files to the cloud storage
